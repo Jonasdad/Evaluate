f05f71ff98c736710e714e6e645b6aab827dd177
Jonasdad <Jonasdad@kth.se>
2023-01-28 20:59:49 +0100

    Wonky code

--- Evaluate.ex
+++ Evaluate.ex
@@ -1,6 +1,3 @@
-#(2x + 3) + 1/2
-#{:add, {:add, {:mul, {:num, 2}, {:var, :x}}, {:num, 3}}, {:q, 1,2}}
-
 defmodule Evaluate do
   @type literal() :: {:num, number()}
   @type expr() :: {:add, expr(), expr()}
@@ -8,9 +5,6 @@ defmodule Evaluate do
                 | {:mul, expr(), expr()}
                 | {:div, expr(), expr()}
                 | literal()
-
-#expr: {:num, :c}
-#tree: {:node, :c, 4 }.... etc
   def test() do
     env = EnvTree.new()
     env = EnvTree.add(:nil, :a, 3)
@@ -18,12 +12,33 @@ defmodule Evaluate do
     env = EnvTree.add(env, :d, 1)
     env = EnvTree.add(env, :b, 2)
     env = EnvTree.add(env, :m, 5)
- #   expr = {:add, {:add, {:num, :a}, {:num, :b}}, {:add, {:num, :c}, {:num, :d}}}
-    expr = {:mul, {:div, {:mul, {:num, :c}, {:num, :d}}, {:add, {:num, :a}, {:num, :b}}}, {:num, :m}}
-    eval(expr, env)
+    env = EnvTree.add(env, :p, 12)
+    env = EnvTree.add(env, :u, 7)
+    #expr = {:mul, {:div, {:add, {:add, {:num, :a}, {:num, :b}}, {:add, {:num, :c}, {:num, :d}}}, {:num, :c}}, {:num, :b}}
+    #a/(d+c) * (m+a) = (3/5) * (8) = 24/5
+    #(3)/(6) * 5 = 15 / 6
+    #((5*7) + (3*12)) / 4 = (35) + 36 / 4 = 71 / 4
+
+        #rad 22 fungerar utan rad 71
+   # expr = {:div, {:add, {:mul, {:num, :m}, {:num, :m}}, {:mul, {:num, :m}, {:num, :m}}}, {:num, :m}}
+        #rad 23 fungerar med rad 71
+   expr = {:mul, {:div, {:num, :a}, {:add, {:num, :b}, {:num, :c}}}, {:add, {:num, :b}, {:num, :a}}}
+   # expr = {:mul, {:mul, {:add, {:num, :a}, {:num, :b}}, {:num, :m}}, {:num, :b}}
+   #expr = {:add, {:num, :a}, {:num, :m}}
+  # expr = {:mul, {:div, {:num, :a}, {:num, :b}}, {:num, :a}}
+   IO.write("Original Expression:  #{Evaluate.simplify(expr)}\n")
+#   IO.write("Expression with substitution #{Evaluate.simplify(EnvTree.lookup(env, expr))}\n")
+   IO.write("Evaluation with substitution:  #{(Evaluate.eval(expr, env))}\n")
+   #eval(expr, env)
   end
 
-  #Create an environment (tree or linked list) with an atom as :var and value as :num
+
+  def simplify({:num, n}) do "#{n}" end
+  def simplify({:add, expr1, expr2}) do "(#{simplify(expr1)} + #{simplify(expr2)})" end
+  def simplify({:mul, expr1, expr2}) do "#{simplify(expr1)} * #{simplify(expr2)}" end
+  def simplify({:div, expr1, expr2}) do "(#{simplify(expr1)} / #{simplify(expr2)})" end
+  def simplify({:q, expr1, expr2}) do "(#{simplify(expr1)} / #{simplify(expr2)}" end
+   #Create an environment (tree or linked list) with an atom as :var and value as :num
   #use eval to evaluate expressions - Each atom will have an assigned value to it.
 
   def eval({:num, n}, env) do EnvTree.lookup(env, n) end
@@ -32,23 +47,34 @@ defmodule Evaluate do
 
   def eval({:add, expr1, expr2}, env) do
     add(eval(expr1, env), eval(expr2, env))
-    #    eval(add(EnvTree.lookup(env, expr1), EnvTree.lookup(env, expr2)), env)
   end
   def eval({:mul, expr1, expr2}, env) do
+    #if(expr1 = {:div, nom, denom}) do
+     # Evaluate.div(mul(eval(nom, env), eval(expr2, env)), eval(denom, env))
+    #end
     mul(eval(expr1, env), eval(expr2, env))
   end
 
   def eval({:div, expr1, expr2}, env) do
     if(rem(eval(expr1, env), eval(expr2, env)) == 0) do
-      Evaluate.div(eval(expr1, env), eval(expr2, env))
-      eval(Evaluate.div(EnvTree.lookup(env, expr1), EnvTree.lookup(env, expr2)), env)
-    else "(#{eval(expr1, env)}/#{eval(expr2, env)})"
+      trunc(Evaluate.div(eval(expr1, env), eval(expr2, env)))
+  #  else "#{eval(expr1, env)} / #{eval(expr2, env)}"
+      else {:div, eval(expr1, env), eval(expr2, env)}
+      end
     end
-  end
 
-  def add(expr1, expr2) do expr1 + expr2 end
-  def mul(expr1, expr2) do expr1 * expr2 end
-  def div(expr1, expr2) do expr1 / expr2 end
-  def div(expr1, 0) do :error end
+ # def eval({:mul, {:q, expr1, expr2}, expr3}) do Evaulate.div(mul(expr1, expr3), expr2) end
 
+
+ def add(expr1, expr2) do expr1 + expr2 end
+ def mul({type, n, d}, expr1) when type == :div do Evaluate.div({:num, n*expr1}, {:num, d}) end
+ def mul(expr1, expr2) do expr1 * expr2 end
+
+ def div({:num, nom}, {:num, denom}) do
+  if(rem(nom, denom) == 0) do
+    trunc(nom / denom)
+  else "#{nom} / #{denom}"
+    end
+   end
+ def div(expr1, expr2) do expr1 / expr2 end
 end
