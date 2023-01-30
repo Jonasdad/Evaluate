97145b674ba12be986b2c87d1187a1b82054b80d
Jonasdad <Jonasdad@kth.se>
2023-01-30 10:12:19 +0100

    Fixed small bug

--- Evaluate.ex
+++ Evaluate.ex
@@ -17,8 +17,10 @@ defmodule Evaluate do
 
    #expr = {:div, {:add, {:mul, {:num, :m}, {:num, :m}}, {:mul, {:num, :m}, {:num, :m}}}, {:num, :m}}
    #expr = {:mul, {:div, {:num, :a}, {:add, {:num, :b}, {:num, :c}}}, {:add, {:num, :b}, {:num, :a}}}
-   #4/7 * 2
-   expr = {:mul, {:div, {:num, :c}, {:num, :u}}, {:num, :u}}
+   #35 / 3
+   #expr = {:div, {:mul, {:num, :m}, {:num, :u}}, {:num, :a}}
+   #3/12 * 7 = 7/4
+   expr = {:mul, {:div, {:num, :a}, {:num, :p}}, {:num, :u}}
    IO.write("Original Expression:  #{Evaluate.simplify(expr)}\n")
    IO.write("Evaluation with substitution:  #{(Evaluate.eval(expr, env))}\n")
   end
@@ -28,9 +30,6 @@ defmodule Evaluate do
   def simplify({:add, expr1, expr2}) do "(#{simplify(expr1)} + #{simplify(expr2)})" end
   def simplify({:mul, expr1, expr2}) do "#{simplify(expr1)} * #{simplify(expr2)}" end
   def simplify({:div, expr1, expr2}) do "(#{simplify(expr1)} / #{simplify(expr2)})" end
-  def simplify({:q, expr1, expr2}) do "(#{simplify(expr1)} / #{simplify(expr2)}" end
-   #Create an environment (tree or linked list) with an atom as :var and value as :num
-  #use eval to evaluate expressions - Each atom will have an assigned value to it.
 
   def eval({:num, n}, env) do EnvTree.lookup(env, n) end
 
@@ -44,28 +43,28 @@ defmodule Evaluate do
     mul(eval(expr1, env), eval(expr2, env))
   end
 
+  #Special case when demoninator is larger AND a factor of nominator
   def eval({:div, expr1, expr2}, env) when expr2 > expr1 do
     if(rem(eval(expr2, env), eval(expr1, env)) == 0) do
         ({:div, 1, trunc(eval(expr2, env)/eval(expr1, env))})
-      else {:div, eval(expr1,env), eval(expr2, env)}
+      else divi(eval(expr1,env), eval(expr2, env))
     end
   end
 
   def eval({:div, expr1, expr2}, env) do
-    if(rem(eval(expr1, env), eval(expr2, env)) == 0) do
-            divi(eval(expr1, env), eval(expr2, env))
-  #  else "#{eval(expr1, env)} / #{eval(expr2, env)}"
-      else {:div, eval(expr1, env), eval(expr2, env)}
-      end
+    divi(eval(expr1, env), eval(expr2, env))
     end
 
 
- # def eval({:mul, {:q, expr1, expr2}, expr3}) do Evaulate.div(mul(expr1, expr3), expr2) end
-# test 123
+  # def eval({:mul, {:q, expr1, expr2}, expr3}) do Evaulate.div(mul(expr1, expr3), expr2) end
+  # test 123
 
  def add(expr1, expr2) do expr1 + expr2 end
+ #Special case multiplication of rational number and integer: a/b * c return a*c / b and simplify
  def mul({:div, expr1, expr2}, expr3) do divi(expr1*expr3, expr2) end
  def mul(expr1, expr2) do expr1 * expr2 end
+ #if interger division is ok do nom / denom
+ #else return the rational
  def divi(nom, denom) do
   if(rem(nom, denom) == 0) do
     trunc(nom / denom)
